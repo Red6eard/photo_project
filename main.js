d8351559910e2145c736d39856a0dcf13f9027ec
Red6eard <vadim.71rus@gmail.com>
2023-04-05 08:35:56 +0300

    добавлена функция прокрутки слайдера вперед

---
+++ main.js
@@ -0,0 +1,26 @@
+
+//выбираем элементы на странице
+const photo = document.getElementById("photo");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+//сщздаем массив с сылками на фотки
+
+const photoArr = ['image/Nik1Avers.jpg','image/Nik1Revers.jpg','image/Nik2Avers.jpg','image/Nik2Revers.jpg'];
+
+//функция прокрутки слайдера вперед
+let count = 0;
+function getNext() {
+    count += 1;
+    if (count > photoArr.length - 1) {
+        count = 0;
+    }
+    return count;
+};
+
+//функция прокрутки слайдера назад
+function getPrev() {
+    
+}
+
+console.log(count);
