18293e7883326d775292130b3bc53f52fe464985
Red6eard <vadim.71rus@gmail.com>
2023-04-14 21:26:00 +0300

    без изменений

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const photo = document.getElementById("photo");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 
-//сщздаем массив с сылками на фотки
+//создаем массив с сылками на фотки
 
 const photoArr = ['image/Nik1Avers.jpg','image/Nik2Avers.jpg','image/Nik1Revers.jpg','image/Nik2Revers.jpg','image/masAvers.jpg','image/masRevers.jpg','image/GrivAvers.jpg','image/GrivRevers.jpg'];
 
@@ -28,7 +28,7 @@ function getPrev() {
 };
 
 
-//прос/*  */лушиватель событий на кнопку вперед
+//прослушиватель событий на кнопку вперед
 
 next.addEventListener("click",getNext);
 
